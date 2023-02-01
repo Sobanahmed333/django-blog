f972a45914453e3697be913b91599f7831c51a65
Soban Ahmed <sobanahmed333@gmail.com>
2023-02-01 17:25:28 +0500

    Added postgres

--- blog/settings.py
+++ blog/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', **env.db(),
+    } if env.str("DATABASE_URL", default=None) else {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
