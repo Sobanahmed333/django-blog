25dd354d5a09e2f7f2942cbb8d9cc48c0e284627
Soban Ahmed <sobanahmed333@gmail.com>
2023-02-01 18:18:12 +0500

    Create Post model

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseTimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+
+class Post(BaseTimeStamp):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='post_created_by')
+
+    REQUIRED_FIELDS = ['title', 'description']
+
+    def __str__(self):
+        return self.title
