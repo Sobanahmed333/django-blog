d65996196f562d3c47f959035e6fea7cb87601cf
Soban Ahmed <sobanahmed333@gmail.com>
2023-02-02 18:23:48 +0500

    Added Comment model and endpoints

--- apps/post/api/v1/viewsets.py
+++ apps/post/api/v1/viewsets.py
@@ -1,11 +1,17 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
-from apps.post.api.v1.serializers import PostSerializer
-from apps.post.models import Post
+from apps.post.api.v1.serializers import PostSerializer, CommentSerializer
+from apps.post.models import Post, Comment
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (AllowAny,)
--- apps/post/models.py
+++ apps/post/models.py
@@ -12,11 +12,19 @@ class BaseTimeStamp(models.Model):
 
 class Post(BaseTimeStamp):
     title = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
     created_by = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='post_created_by')
+    liked_by = models.ManyToManyField('user.User', related_name='post_liked_by', blank=True)
 
     REQUIRED_FIELDS = ['title', 'description']
 
     def __str__(self):
         return self.title
+
+
+class Comment(BaseTimeStamp):
+    created_by = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='comment_created_by')
+    post = models.ForeignKey('post.Post', on_delete=models.CASCADE, related_name='comment_post')
+    context = models.TextField()
+
+    def __str__(self):
+        return f'from: {self.created_by.email} - post: {self.post.title}'
