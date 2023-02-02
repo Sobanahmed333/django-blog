0fa1a1449056ab4232f9fae8d6771b77c8e5aad8
Soban Ahmed <sobanahmed333@gmail.com>
2023-02-02 18:49:29 +0500

    minor fix

--- apps/post/api/v1/urls.py
+++ apps/post/api/v1/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.post.api.v1.viewsets import PostViewSet
+from apps.post.api.v1.viewsets import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 
 router.register('post', PostViewSet, basename='post')
+router.register('comment', CommentViewSet, basename='post')
 
 urlpatterns = [
     path("", include(router.urls)),
